89e7e32dd9c036cb597df8bea56ae8990f2dd29d
rc-earnest <rc.earnest04@gmail.com>
2025-02-16 15:57:10 -0700

    Adds code working in the auto function and explains code in comments

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -1,3 +1,11 @@
+'Rudy Earnest
+'RCET 2265
+'Spring 2025
+'Accumulitive Messages
+'https://github.com/rosstimo/myRepo.git    
+Option Strict On
+Option Explicit On
+Option Compare Text
 
 Imports System
 
@@ -9,9 +17,18 @@ Module MessageProgram
     End Sub
 
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
-        'your code here
-        Return messages
+        Static message As New Text.StringBuilder() 'dims message as a "global variable" and makes it as a string builder class
+
+        If clear Then
+            message.Clear() 'clears the message string builder
+
+        ElseIf Not String.IsNullOrEmpty(newMessage) Then 'checks to make sure that there is something inside of the string
+            message.AppendLine(newMessage) 'writes every string in a newline that is in newMessage to message
+
+        End If
+        Return message.ToString() 'converts the message string builder to string
     End Function
+
 
 
 End Module
